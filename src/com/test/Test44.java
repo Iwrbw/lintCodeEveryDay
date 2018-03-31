1d4706fa02dfd8bc946d3489191fbe978d2eeb7a
yangshunfan <15700734908@163.com>
2018-03-31 20:06:48 +0800

    最小子数组

---
+++ src/com/test/Test44.java
@@ -0,0 +1,38 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create by yangshunfan
+ * 2018/3/31 19:11
+ * 最小子数组
+ */
+public class Test44 {
+    public static int minSubArray(List<Integer> nums) {
+        // write your code here
+        if(nums == null || nums.size() == 0) {
+            return 0;
+        }
+        int sum = nums.get(0);
+        int min = sum;
+        for (int i = 1; i < nums.size(); i++) {
+            if (sum < 0) {
+                sum += nums.get(i);
+            } else {
+                sum = nums.get(i);
+            }
+            min = Math.min(sum,min);
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList();
+        int[] a = {1,1,1,1,1,1,1,1,1,-19,1,1,1,1,1,1,1,-2,1,1,1,1,1,1,1,1,-2,1,-15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+        for (int i = 0; i < a.length; i++) {
+            list.add(a[i]);
+        }
+        System.out.println(minSubArray(list));
+    }
+}
