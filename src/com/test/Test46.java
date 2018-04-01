4ccd8bc0c001e19b95c121fffb6df804ce303c41
yangshunfan <15700734908@163.com>
2018-04-01 21:25:28 +0800

    主元素

---
+++ src/com/test/Test46.java
@@ -0,0 +1,50 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Create by yangshunfan
+ * 2018/4/1 20:05
+ * 主元素
+ */
+public class Test46 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList();
+        int[] a = {2,2,2,1};
+        for (int i = 0; i < a.length; i++) {
+            list.add(a[i]);
+        }
+        System.out.println(majorityNumber(list));
+    }
+
+    public static int majorityNumber(List<Integer> nums) {
+        // write your code here
+        if (nums == null || nums.size() == 0) {
+            return 0;
+        }
+        if (nums.size() == 1) {
+            return nums.get(0);
+        }
+        int[] arr = new int[nums.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = nums.get(i);
+        }
+        Arrays.sort(arr);
+        int temp = arr[0];
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == temp) {
+                result++;
+                if (result > arr.length/2) {
+                    return arr[i];
+                }
+            } else {
+                temp = arr[i];
+            }
+
+        }
+        return 0;
+    }
+}
