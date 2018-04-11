c291307059349f5027ee0c2d3586e1a0b576b0ae
yangshunfan <15700734908@163.com>
2018-04-11 23:24:07 +0800

    二叉树的最小深度

---
+++ src/com/test/Test155.java
@@ -0,0 +1,25 @@
+package com.test;
+
+/**
+ * @author yangshunfan 2018/4/11 22:18
+ * 二叉树的最小深度
+ */
+public class Test155 {
+
+    /**
+     * 最小深度方法
+     * @param root
+     * @return int
+     */
+    public int minDepth(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return 0;
+        }
+
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
+    }
+}
