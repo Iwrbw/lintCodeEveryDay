a4e68f39b2d30ecca9516633e4c988e7cab82f6d
yangshunfan <15700734908@163.com>
2018-04-18 22:32:44 +0800

    10个人出山洞的问题

---
+++ src/GetNameThread.java
@@ -0,0 +1,20 @@
+/**
+ * @author yangshunfan 2018/4/18 15:11
+ */
+public class GetNameThread extends Thread{
+    private ThroughCave tcave;
+    private String name;
+    /**
+     * 初始化线程的时候，获得回调类对象和人名
+     */
+    public GetNameThread(ThroughCave tcave,String name) {
+        this.tcave = tcave;
+        this.name = name;
+    }
+    @Override
+    public void run(){
+        //输出人名，模拟过山洞
+        tcave.printName(name);
+    }
+
+}
---
+++ src/ThroughCave.java
@@ -0,0 +1,41 @@
+/**
+ * @author yangshunfan 2018/4/18 15:11
+ */
+public class ThroughCave {
+    private String[] name;
+
+    private ThroughCave() {
+        //在构造方法中初始化一个包含10个人名的String数组
+        this.name = new String[]{"张三","李四","王五","马六","牛七","王八","小明","小红","小花","小雷"};
+    }
+
+    /**
+     * 输出人名的方法，同步化synchronized
+     * @param name
+     */
+    public synchronized void printName(String name){
+        //延迟5s，模拟过山洞的时间为5s
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(name);
+    }
+    /**
+     * 将10个人名分给10个线程，模拟10个人
+     */
+    private void sendAllNames(){
+        for (String aName : name) {
+            GetNameThread gt = new GetNameThread(this, aName);
+            gt.start();
+
+        }
+    }
+
+    public static void main(String[] args){
+        ThroughCave tc = new ThroughCave();
+        tc.sendAllNames();
+    }
+}
