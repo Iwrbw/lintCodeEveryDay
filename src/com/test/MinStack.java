1a8bd0552a12eaaf1070b87b9636c20c113dd231
yangshunfan <15700734908@163.com>
2018-04-30 15:08:11 +0800

    Test12:实现带最小值方法的栈

---
+++ src/com/test/MinStack.java
@@ -0,0 +1,33 @@
+package com.test;
+
+/**
+ * @author yangshunfan 2018/4/30 13:31
+ * 带最小值操作的栈
+ */
+public class Test12 {
+    public MinStack() {
+        // do intialization if necessary
+    }
+
+    /**
+     * @param number: An integer
+     * @return: nothing
+     */
+    public void push(int number) {
+        // write your code here
+    }
+
+    /**
+     * @return: An integer
+     */
+    public int pop() {
+        // write your code here
+    }
+
+    /**
+     * @return: An integer
+     */
+    public int min() {
+        // write your code here
+    }
+}
